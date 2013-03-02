f1822e92e7547749fe5c690dfc5fd3300989d27b
Zachary Davison <zac.developer@gmail.com>
2013-03-02 16:36:01 +0000

    More FB stuff

--- assets/js/backbone/controllers/playlist_controller.js
+++ assets/js/backbone/controllers/playlist_controller.js
@@ -10,12 +10,13 @@ playlist.ItemController = Backbone.Router.extend({
 
 	initialize: function(options)
 	{
-		_.bindAll(this,"onSearch","onFBLogin","onFBFriendsLoaded");
+		_.bindAll(this,"onSearch","onFBLogin","onFBFriendsLoaded","onFBLinksLoaded");
 
 		this.searchView = new playlist.SearchView({el:".searchView"});
 		this.searchView.bind("playlist:search",this.onSearch);
 		$("body").bind("fb:loginReady",this.onFBLogin);
 		this.friendList = new playlist.FriendList();
+		this.itemList = new playlist.ItemList();
 	},
 
 	showView: function(el)
@@ -34,12 +35,17 @@ playlist.ItemController = Backbone.Router.extend({
 		this.showView(".searchResultsView");
 	},
 
-	onSearch: function(userId)
+	onSearch: function(realName)
 	{
-		FB.api('/' + userId, {
+		//grab userID
+		var user = this.friendList.find(function(item){
+			return item.get("name").toLowerCase().indexOf(realName.toLowerCase()) > -1;
+		});
+
+		FB.api('/' + user.get("id"), {
           fields: 'links'
         },
-        this.onFBFriendsLoaded);
+        this.onFBLinksLoaded);
 	},
 
 	onFBLogin: function(){
@@ -51,6 +57,10 @@ playlist.ItemController = Backbone.Router.extend({
 
 	onFBFriendsLoaded: function(response){
 		this.friendList.reset(response.friends.data);
-		console.log(this.friendList);
+	},
+
+	onFBLinksLoaded: function(response){
+		this.itemList.reset(response.links.data);
+		console.log(response);
 	}
 });
